90492ef5b77543396136c84debd1305d4cc01893
Breno R. <br3eno14@gmail.com>
2024-05-17 17:42:00 -0300

    create animal controller resource

--- src/media/dto/media.dto.ts
+++ src/media/dto/media.dto.ts
@@ -1,4 +1,5 @@
 export class MediaDto {
   url: string;
   mediaType: string;
+  animalId: number;
 }
--- src/media/media.controller.ts
+++ src/media/media.controller.ts
@@ -1,7 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req } from '@nestjs/common';
 import { MediaService } from './media.service';
+import { MediaQueryDto } from './dto/media.query.dto';
+import { MediaDto } from './dto/media.dto';
 
 @Controller('api/medias')
 export class MediaController {
   constructor(private readonly mediaService: MediaService) {}
+
+  @Post()
+  async create(@Body() mediaDto: MediaDto) {
+    return this.mediaService.create(mediaDto);
+  }
+
+  @Get()
+  async find(@Req() req: any) {
+    const mediaQuery: MediaQueryDto = req.query;
+
+    return this.mediaService.consult(mediaQuery);
+  }
 }
