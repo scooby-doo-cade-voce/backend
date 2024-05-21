77e0283c06f3eecf9e7d40da8454c2be7c60b036
Leonardo Simao <leonardosc.me@gmail.com>
2024-05-21 10:02:20 -0300

    chore(document-api): add description api documentation
    Adicionado toda a descrição da API, como schemas e parametros
    
    #20

--- src/size/dto/size.dto.ts
+++ src/size/dto/size.dto.ts
@@ -1,4 +1,8 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class Size {
+  @ApiProperty()
   id: number;
+  @ApiProperty()
   label: string;
 }
--- src/species/dto/species.dto.ts
+++ src/species/dto/species.dto.ts
@@ -1,4 +1,8 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class SpeciesDto {
+  @ApiProperty()
   id: number;
+  @ApiProperty()
   label: number;
 }
